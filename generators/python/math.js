10fe80c64b7983da0b591bbb819a61737dbd61fe
Tim Dawborn <tim.dawborn@gmail.com>
2015-08-30 14:46:53 +1000

    Revert "Added associativity via paren's for nested arithmatic blocks."
    This reverts commit ddf15a6d1268cf481f2469fad3af109fdc2c695e.

--- generators/python/math.js
+++ generators/python/math.js
@@ -52,17 +52,9 @@ Blockly.Python['math_arithmetic'] = function(block) {
   var tuple = OPERATORS[block.getFieldValue('OP')];
   var operator = tuple[0];
   var order = tuple[1];
-  var blockA = block.getInputTargetBlock('A');
-  var argumentA = Blockly.Python.valueToCode(block, 'A', order) || '0';
-  if (blockA.type === 'math_arithmetic') {
-    argumentA = '(' + argumentA + ')';
-  }
-  var blockB = block.getInputTargetBlock('B');
-  var argumentB = Blockly.Python.valueToCode(block, 'B', order) || '0';
-  if (blockB.type === 'math_arithmetic') {
-    argumentB = '(' + argumentB + ')';
-  }
-  var code = argumentA + operator + argumentB;
+  var argument0 = Blockly.Python.valueToCode(block, 'A', order) || '0';
+  var argument1 = Blockly.Python.valueToCode(block, 'B', order) || '0';
+  var code = argument0 + operator + argument1;
   return [code, order];
   // In case of 'DIVIDE', division between integers returns different results
   // in Python 2 and 3. However, is not an issue since Blockly does not
